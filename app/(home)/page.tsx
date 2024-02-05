080532e087fde36d6e64fca692181c440827784a
Fabio Zanardo <shadomhp@gmail.com>
2024-02-04 21:19:19 -0300

    feat: add initial barbershop item

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,28 +3,41 @@ import Header from "../_components/header";
 import { ptBR } from 'date-fns/locale';
 import Search from './_components/search';
 import BookingItem from '../_components/booking-item';
+import { db } from '../_lib/prisma';
+import BarbershopItem from './_components/barbershop-item';
 
-export default function Home() {
-  return (
-    <div>
-      <Header />
+export default async function Home() {
+    const barbershops = await db.barbershop.findMany({})
 
-      <div className='px-5 pt-5'>
-        <h2 className='text-xl font-bold'>Olá, Miguel!</h2>
-        <p className='capitalize text-sm'>
-          {format(new Date(), "EEEE', 'dd 'de ' MMMM ", {
-            locale: ptBR,
-          })}
-        </p>
-      </div>
-      <div className="px-5 mt-6">
-        <Search />
-      </div>
-      <div className="px-5 mt-6">
-          <h2 className='text-xs mb-3 uppercase text-gray-400 font-bold '>Agendamentos</h2>
-        <BookingItem />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+
+            <div className='px-5 pt-5'>
+                <h2 className='text-xl font-bold'>Olá, Miguel!</h2>
+                <p className='capitalize text-sm'>
+                    {format(new Date(), "EEEE', 'dd 'de ' MMMM ", {
+                        locale: ptBR,
+                    })}
+                </p>
+            </div>
+            <div className="px-5 mt-6">
+                <Search />
+            </div>
+            <div className="px-5 mt-6">
+                <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+                <BookingItem />
+            </div>
+            <div className="mt-6">
+                <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+                <div className='flex px-5 gap-2 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
+                    {barbershops.map((barbershop) => (
+                        <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+                    ))}
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
