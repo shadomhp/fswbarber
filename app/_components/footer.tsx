114bf3ae6c1e6bef4803d96ad3813d3e953811f4
Fabio Zanardo <shadomhp@gmail.com>
2024-02-04 21:29:01 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="w-ful bg-secondary py-6 px-5">
+            <p className="text-gray-400 text-xs font-bold opacity-75">© 2023 Copyright FSW Barbershow</p>
+        </div>
+     );
+}
+ 
+export default Footer;
